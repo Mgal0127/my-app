3e70bae35374486bc81afd9b3930738c8d9521d4
Mikhail <mgal0127@gmail.com>
2020-06-22 17:39:47 +0100

    Question 2

--- src/pages/Question2.js
+++ src/pages/Question2.js
@@ -2,10 +2,42 @@ import React, {  useCallback} from 'react';
 
 const Question2 = () => {
 
+  const [inputValue, setInputValue] = React.useState("");
+  const [countryList, setCountryList] = React.useState("");
+  const [flag, setFlag] = React.useState("");
+
+  const handleChange = event => {
+    setInputValue(event.target.value);
+  };
+
+  const sendRequest = useCallback(async () => {
+      const response = await fetch(`http://localhost:8000/api/getCountries/${inputValue}/false`);
+      const body = await response.json();
+      setCountryList(body);
+      setFlag(true);
+  },)
+
+
   return (
     <>
-      <h1>Welcome to Question 2 of the Exercise</h1>
+      <h1>Welcome to My New Page</h1>
 
+      <br></br>
+
+      <input onChange={handleChange}
+        value={inputValue}></input>
+
+      <br></br>
+
+      <button onClick={sendRequest}>Search for Countries</button>
+
+      <div className='form-container'>
+        {(flag) ?
+          countryList.map((country, index) => (
+            <li key={index}>{country.name} - Capital City Name: {country.capital}</li>
+          ))
+          : ''}
+      </div>
     </>
     );
 }
